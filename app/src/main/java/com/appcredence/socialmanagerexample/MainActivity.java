4385cc34b2b47a156284c118587f82dc80a84d0f
Swapnil N <swapnil.nandgave@celusion.com>
2019-03-13 13:26:00 +0530

    Added Implementation of Library

--- app/src/main/java/com/appcredence/socialmanagerexample/MainActivity.java
+++ app/src/main/java/com/appcredence/socialmanagerexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.appcredence.socialmanagerexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.appcredence.socialmanager.SocialUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
